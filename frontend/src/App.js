50a5513fbafc9e3670d29302ac1541e4a79254eb
Dano <danielaherniv@gmail.com>
2020-11-15 21:45:25 -0500

    5   9:31

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import {Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Pizza Academy</h1>
+    <main>
+      <Header/>
+      <Container>
+        <h1>Yeah Buddy</h1>
+      </Container>
+      <Footer/>
+
+    </main>
     </>
   );
 }
